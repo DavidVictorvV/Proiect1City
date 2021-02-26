23a560570d77e7d0d37ee0cf8b4575258097390e
Van Velden David Victor <davidvictorvanvelden@gmail.com>
2021-02-26 14:53:19 +0200

    first commit

---
+++ City/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace City
+{
+
+    public abstract class Building
+    {
+        public  int houseSize;
+        public  Boolean parking = false;
+
+        public Building(int houseSize)
+        {
+            this.houseSize = houseSize;
+            if (houseSize => 20)
+            {
+                parking = true;
+            }
+        }
+
+        public abstract Boolean canPark();
+        
+
+    }   
+
+    public class House : Building
+    {
+        public House(int houseSize) : base(houseSize)
+        {
+        }
+
+        public override bool canPark()
+        {
+            if (parking)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           House h = new House(54);
+            Console.WriteLine(h.canPark());
+        }
+    }
+}
